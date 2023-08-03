a138c7403d64c2dfaa60a5ac0c602f3716d77815
Ipuut <99790646+Nafisaputrihanifah@users.noreply.github.com>
2023-08-03 15:26:36 +0700

    Add files via upload

---
+++ src/assets/kategori.js
@@ -0,0 +1,29 @@
+export const kategori = {
+    "kategori": [
+        {
+            "id" : 1,
+            "nama" : "Elektonik",
+            "img" : "elektronik.jpg"
+        },
+        {
+            "id" : 2,
+            "nama" : "Alat Make Up",
+            "img" : "alat make up.jpg "
+        },
+        {
+            "id" : 3,
+            "nama" : "Set Alat Makan",
+            "img" : "alat makan.jpg"
+        },
+        {
+            "id" : 4,
+            "nama" : "Pakaian",
+            "img" : "pakaian.jpg"
+        },
+        {
+            "id" : 5,
+            "nama" : "Celana",
+            "img" : "celana.jpg"
+        },
+    ]
+}
---
+++ src/assets/produk.js
@@ -0,0 +1,110 @@
+export const produk = {
+    "produk": [
+        {
+            "id" : 1,
+            "nama" : "handphone",
+            "harga" : "Rp 1.000.000,00",
+            "img" : "hp.jpg",
+            "id_kategori": 1
+           
+        },
+        {
+            "id" : 2,
+            "nama" : "Laptop",
+            "harga" : "Rp 5.000.000,00",
+            "img" : "laptop.jpg",
+            "id_kategori": 1
+        },
+        {
+            "id" : 3,
+            "nama" : "TV",
+            "harga" : "Rp 5.000.000,00",
+            "img" : "tv.jpg",
+            "id_kategori": 1
+        },
+        {
+            "id" : 4,
+            "nama" : "Bedak",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "bedak.jpg",
+            "id_kategori": 2
+        },
+        {
+            "id" : 5,
+            "nama" : "Lipstik",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "Lipstik.jpg",
+            "id_kategori": 2
+        },
+        {
+            "id" : 6,
+            "nama" : "Concealer",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "concealer.jpg",
+            "id_kategori": 2
+        },
+        {
+            "id" : 7,
+            "nama" : "Sendok",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "sendok.jpg",
+            "id_kategori": 3
+        },
+        {
+            "id" : 8,
+            "nama" : "Garpu",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "garpu.jpg",
+            "id_kategori": 3
+        },
+        {
+            "id" : 9,
+            "nama" : "Piring",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "piring.jpg",
+            "id_kategori": 3
+        },
+        {
+            "id" : 10,
+            "nama" : "Baju Tidur",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "baju tidur.jpg",
+            "id_kategori": 4
+        },
+        {
+            "id" : 11,
+            "nama" : "Jaket",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "jaket.jpg",
+            "id_kategori": 4
+        },
+        {
+            "id" : 12,
+            "nama" : "Sweater",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "sweater.jpg",
+            "id_kategori": 4
+        },
+        {
+            "id" : 13,
+            "nama" : "Celana Kargo",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "kargo.jpg",
+            "id_kategori": 5
+        },
+        {
+            "id" : 14,
+            "nama" : "Celana Jeans",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "jeans.jpg",
+            "id_kategori": 5
+        },
+        {
+            "id" : 13,
+            "nama" : "Celana Kulot",
+            "harga" : "Rp 3.000.000,00",
+            "img" : "kulot.jpg",
+            "id_kategori": 5
+        },
+    ]
+}
